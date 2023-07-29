d2bd1142e5efb466a1e8d2a32f06f5efd6008fce
17862719591 <184974699@qq.com>
2023-07-29 21:10:36 +0800

    测试文件设置为过期

--- src/main/java/org/tinycloud/jdbc/sql/UploadFile.java
+++ src/main/java/org/tinycloud/jdbc/sql/UploadFile.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Deprecated
 @Table("b_upload_file")
 public class UploadFile implements Serializable {
     private static final long serialVersionUID = -1L;
