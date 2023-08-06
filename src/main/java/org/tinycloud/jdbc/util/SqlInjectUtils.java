81254a39e3ff2ccf8830928f914ab3ea4c856bf5
17862719591 <184974699@qq.com>
2023-08-06 21:20:59 +0800

    feat: 增加几个常用的公共方法

---
+++ src/main/java/org/tinycloud/jdbc/util/SqlInjectUtils.java
@@ -0,0 +1,46 @@
+package org.tinycloud.jdbc.util;
+
+import org.springframework.util.Assert;
+
+import java.util.regex.Pattern;
+
+/**
+ * sql注入检测工具类
+ *
+ * @author liuxingyu01
+ * @since 2023-08-05
+ **/
+public class SqlInjectUtils {
+    /**
+     * SQL语法检查正则：符合两个关键字（有先后顺序）才算匹配
+     */
+    private static final Pattern SQL_SYNTAX_PATTERN = Pattern.compile("(insert|delete|update|select|create|drop|truncate|grant|alter|deny|revoke|call|execute|exec|declare|show|rename|set)" +
+            "\\s+.*(into|from|set|where|table|database|view|index|on|cursor|procedure|trigger|for|password|union|and|or)|(select\\s*\\*\\s*from\\s+)|(and|or)\\s+.*(like|=|>|<|in|between|is|not|exists)", Pattern.CASE_INSENSITIVE);
+    /**
+     * 使用'、;或注释截断SQL检查正则
+     */
+    private static final Pattern SQL_COMMENT_PATTERN = Pattern.compile("'.*(or|union|--|#|/\\*|;)", Pattern.CASE_INSENSITIVE);
+
+    /**
+     * 检查参数是否存在 SQL 注入
+     *
+     * @param value 检查参数
+     * @return true 非法， false 合法
+     */
+    public static boolean check(String value) {
+        Assert.notNull(value, "所传入的SQL参数不能为空！");
+        // 处理是否包含SQL注释字符 || 检查是否包含SQL注入敏感字符
+        return SQL_COMMENT_PATTERN.matcher(value).find() || SQL_SYNTAX_PATTERN.matcher(value).find();
+    }
+
+    /**
+     * 刪除字段转义符单引号双引号
+     *
+     * @param text 待处理字段
+     * @return 处理后的字段
+     */
+    public static String removeEscapeCharacter(String text) {
+        Assert.notNull(text, "所传入参数不能为空！");
+        return text.replaceAll("\"", "").replaceAll("'", "");
+    }
+}
--- src/main/java/org/tinycloud/jdbc/util/StrUtils.java
+++ src/main/java/org/tinycloud/jdbc/util/StrUtils.java
@@ -1,8 +1,5 @@
 package org.tinycloud.jdbc.util;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 /**
  * 字符串工具类
@@ -11,46 +8,108 @@ import java.util.regex.Pattern;
  * @since 2023-08-03
  **/
 public class StrUtils {
-    private static final Pattern humpPattern = Pattern.compile("[A-Z]");
-    private static final Pattern linePattern = Pattern.compile("_(\\w)");
+
+    /**
+     * 下划线字符
+     */
+    public static final char UNDERLINE = '_';
+
+    /**
+     * 判断字符串中是否全是空白字符
+     *
+     * @param cs 需要判断的字符串
+     * @return 如果字符串序列是 null 或者全是空白，返回 true
+     */
+    public static boolean isBlank(CharSequence cs) {
+        if (cs != null) {
+            int length = cs.length();
+            for (int i = 0; i < length; i++) {
+                if (!Character.isWhitespace(cs.charAt(i))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isNotBlank(CharSequence cs) {
+        return !isBlank(cs);
+    }
+
+    public static boolean isEmpty(CharSequence cs) {
+        return cs == null || cs.length() == 0;
+    }
+
+    public static boolean isNotEmpty(CharSequence cs) {
+        return !isEmpty(cs);
+    }
 
     /**
      * 驼峰转下划线 humpToLine("helloWorld") = "hello_world"
      *
-     * @param str 字符串
+     * @param param 字符串
      * @return 字符串
      */
-    public static String humpToLine(String str) {
-        Matcher matcher = humpPattern.matcher(str);
-        StringBuffer sb = new StringBuffer();
-        while (matcher.find()) {
-            matcher.appendReplacement(sb, "_" + matcher.group(0).toLowerCase());
+    public static String humpToLine(String param) {
+        if (isBlank(param)) {
+            return "";
         }
-        matcher.appendTail(sb);
+        int len = param.length();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c = param.charAt(i);
+            if (Character.isUpperCase(c) && i > 0) {
+                sb.append(UNDERLINE);
+            }
+            sb.append(Character.toLowerCase(c));
+        }
         return sb.toString();
     }
 
     /**
      * 下划线转驼峰 lineToHump("hello_world") = "helloWorld"
      *
-     * @param str 字符串
+     * @param param 字符串
      * @return 字符串
      */
-    public static String lineToHump(String str) {
-        if (null == str || "".equals(str)) {
-            return str;
+    public static String lineToHump(String param) {
+        if (isBlank(param)) {
+            return "";
         }
-        str = str.toLowerCase();
-        Matcher matcher = linePattern.matcher(str);
-        StringBuffer sb = new StringBuffer();
-        while (matcher.find()) {
-            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
+        String temp = param.toLowerCase();
+        int len = temp.length();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c = temp.charAt(i);
+            if (c == UNDERLINE) {
+                if (++i < len) {
+                    sb.append(Character.toUpperCase(temp.charAt(i)));
+                }
+            } else {
+                sb.append(c);
+            }
         }
-        matcher.appendTail(sb);
-
-        str = sb.toString();
-        str = str.substring(0, 1).toUpperCase() + str.substring(1);
-
-        return str;
+        return sb.toString();
     }
+
+
+    /**
+     * 首字母转换小写
+     *  Thus "FooBah" becomes "fooBah" and "X" becomes "x", but "URL" stays as "URL".
+     * @param name 需要转换的字符串
+     * @return 转换好的字符串
+     */
+    public static String decapitalize(String name) {
+        if (name == null || name.length() == 0) {
+            return name;
+        }
+        if (name.length() > 1 && Character.isUpperCase(name.charAt(1)) &&
+                Character.isUpperCase(name.charAt(0))){
+            return name;
+        }
+        char[] chars = name.toCharArray();
+        chars[0] = Character.toLowerCase(chars[0]);
+        return new String(chars);
+    }
+
 }
