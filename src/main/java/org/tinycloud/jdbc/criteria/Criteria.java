21fced6f929a732cf112eee04213f0080f482360
17862719591 <184974699@qq.com>
2023-09-03 14:57:25 +0800

    style: 删除掉无用的引入

--- src/main/java/org/tinycloud/jdbc/criteria/Criteria.java
+++ src/main/java/org/tinycloud/jdbc/criteria/Criteria.java
@@ -1,7 +1,5 @@
 package org.tinycloud.jdbc.criteria;
 
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
--- src/main/java/org/tinycloud/jdbc/criteria/LambdaCriteria.java
+++ src/main/java/org/tinycloud/jdbc/criteria/LambdaCriteria.java
@@ -1,9 +1,6 @@
 package org.tinycloud.jdbc.criteria;
 
 
-import org.tinycloud.jdbc.sql.UploadFile;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
