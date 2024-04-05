078080df6f0c77bc8990be9f0b39dd2b55c70533
17862719591 <184974699@qq.com>
2024-04-05 20:33:17 +0800

    style: 格式化代码

--- src/main/java/org/tinycloud/jdbc/BaseDao.java
+++ src/main/java/org/tinycloud/jdbc/BaseDao.java
@@ -5,13 +5,26 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.tinycloud.jdbc.page.IPageHandle;
 import org.tinycloud.jdbc.support.AbstractSqlSupport;
 
+/**
+ * <p>
+ * baseDao基础类，dao层继承该接口即可获得增强的CURD功能
+ * </p>
+ * <p>
+ * 原理：
+ * Spring的AutowiredAnnotationBeanPostProcessor.AutowiredFieldElement.inject会对一个类的本身的字段和其所有父类的字段进行遍历，
+ * 凡是含有@Autowired的字段都会被注入。
+ * </p>
+ *
+ * @param <T>
+ * @param <ID>
+ */
 public class BaseDao<T, ID> extends AbstractSqlSupport<T, ID> {
 
     /**
      * JdbcTemplate
      */
     @Autowired
-    private  JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
 
     /**
      * 分页处理
